6c30b8a08a56eebc361726fb0b2a407fd0abe374
VileshRanjanSringeri <vileshranjan@gmail.com>
2025-09-17 00:36:32 +0530

    Added Best Time to Buy and Sell Stock solution

--- Arrays/BuySellStock.java
+++ Arrays/BuySellStock.java
@@ -1,8 +1,15 @@
+<<<<<<< HEAD
 class Solution {
     public int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
 
+=======
+public class BuySellStock {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+>>>>>>> 93affaf (Added Best Time to Buy and Sell Stock solution)
         for (int price : prices) {
             if (price < minPrice) {
                 minPrice = price;
@@ -12,4 +19,13 @@ class Solution {
         }
         return maxProfit;
     }
+<<<<<<< HEAD
+=======
+
+    public static void main(String[] args) {
+        BuySellStock solution = new BuySellStock();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println("Max Profit: " + solution.maxProfit(prices));
+    }
+>>>>>>> 93affaf (Added Best Time to Buy and Sell Stock solution)
 }
